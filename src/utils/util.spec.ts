ac4f5e668b9ae9a11d7082e8aa1a7fa0de19c194
jeyofdev <jgregoire.pro@gmail.com>
2023-04-30 18:32:12 +0200

    test

--- src/utils/util.spec.ts
+++ src/utils/util.spec.ts
@@ -5,6 +5,6 @@ import sum from './index';
 describe('test helpers', () => {
 	it('return the addition between 2 numbers', () => {
 		expect(sum(1, 2)).toBe(3);
-		expect(sum(1, 2)).not.toBe(5);
+		expect(sum(1, 2)).not.toBe(3);
 	});
 });
