bee1528e871a93d52b60f67da40a2e96dbf6f241
jeyofdev <jgregoire.pro@gmail.com>
2023-04-30 10:38:47 +0200

    feat : init test with jest

---
+++ src/utils/util.spec.ts
@@ -0,0 +1,8 @@
+import sum from './index';
+
+describe('test helpers', () => {
+	it('return the addition between 2 numbers', () => {
+		expect(sum(1, 2)).toBe(3);
+		expect(sum(1, 2)).not.toBe(5);
+	});
+});
